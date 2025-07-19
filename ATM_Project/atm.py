dd4a4a4ae861cc46545949d0e7e561adf6ea5143
2Ho0 <mwilliam55@naver.com>
2025-07-19 15:35:57 +0900

    Bank class

--- ATM_Project/atm.py
+++ ATM_Project/atm.py
@@ -4,7 +4,7 @@ class Account:
         self.balance = balance
     
     def deposit(self, amount):
-        if amount > 0:
+        if amount <= 0:
             raise ValueError("Deposit amount must be positive")
         self.balance += amount
     
@@ -18,3 +18,18 @@ class Account:
     
     def get_balance(self):
         return self.balance
+
+class Bank:
+    def __init__(self):
+        self.cards = {}
+    
+    def add_card(self, card_id, pin, accounts):
+        self.cards[card_id] = {"pin": pin, "accounts": accounts}
+    
+    def verify_card(self, card_id, input_pin):
+        return self.cards.get(card_id, {}).get("pin")==input_pin
+    
+    def get_accounts(self, card_id):
+        if card_id not in self.cards:
+            raise ValueError("Card not found")
+        return self.cards[card_id]["accounts"]
